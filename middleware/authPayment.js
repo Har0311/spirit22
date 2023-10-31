333f4647a084aedcb832050d5c2b19b24efc410e
Harish Kumar <harishkumarnit03112002@gmail.com>
2023-10-31 09:42:43 +0530

    harish

---
+++ middleware/authPayment.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import client_model from "../model/cliend_model.js";
+import alert from "alert";
+
+const authPayment = async (req, res, next) => {
+    try {
+        const token = req.cookies.spirit;
+        const verifyUser = jwt.verify(token, process.env.JWT_SECREAT_KEY);
+        if (!verifyUser) {
+            res.send("please login before payment")
+        } else {
+            const client = await client_model.findOne({ _id: verifyUser._id });
+            if (client.payment_status == "unpaid") {
+               
+               res.redirect('/payments')
+                
+            }
+           
+        }
+
+
+        next();
+    } catch (error) {
+        res.send('err');
+        console.log(error);
+
+    }
+}
+
+export default authPayment;
